1d51b13238670ebf83d0656f0687f20af6672548
yuekcc <yuekcc@qq.com>
2025-06-16 23:43:20 +0800

    增加 launch

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ set -ex
 
 sources=(
     "open.c3"
+    "launch.c3"
 )
 
 for src_file in ${sources[@]};
 do
     tool_name="${src_file%.*}"
+    echo "build tool: ${tool_name}"
     c3c compile -Oz -g0 -o dist/$tool_name $src_file win.c3
 done
 
