83d56602efafde643ea7b82aa880c12419c8cc04
yuekcc <yuekcc@qq.com>
2025-06-18 00:27:45 +0800

    更新配置

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ for src_file in ${sources[@]};
 do
     tool_name="${src_file%.*}"
     echo "build tool: ${tool_name}"
-    c3c compile -Oz -g0 -o dist/$tool_name $src_file win.c3
+    c3c compile -O3 -g0 -o dist/$tool_name $src_file win.c3
 done
 
