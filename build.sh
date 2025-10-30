b0c5b363867f0e593a9b5069ff24e2ec5f22d4b0
yuekcc <yuekcc@qq.com>
2025-10-31 00:37:05 +0800

    参考std库的实现

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ for src_file in ${sources[@]};
 do
     tool_name="${src_file%.*}"
     echo "build tool: ${tool_name}"
-    c3c compile -O3 -g0 -o dist/$tool_name $src_file win.c3 cmd.c3
+    c3c compile -Oz -o dist/$tool_name $src_file win.c3 cmd.c3
 done
