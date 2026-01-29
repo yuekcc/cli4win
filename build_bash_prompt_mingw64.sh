815919a7d7bdfcdbbb5dae6c8b8988a83f359af1
yuekcc <yuekcc@qq.com>
2026-01-30 00:30:22 +0800

    fix 输出超出缓存时会卡不展示

--- build_bash_prompt_mingw64.sh
+++ build_bash_prompt_mingw64.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+OUTPUT=bash_prompt
+
 c3c compile-only bash_prompt.c3 cmd.c3 --target mingw-x64 --single-module=yes -O2
-gcc -o bash_prompt_mingw ./obj/mingw-x64/bash_prompt.obj -ldbghelp -lshlwapi
+gcc -o $OUTPUT ./obj/mingw-x64/bash_prompt.obj -ldbghelp -lshlwapi
+strip -s ${OUTPUT}.exe
