49a43a78c469cde9b17a631b8576a96fbf06100e
yuekcc <yuekcc@qq.com>
2026-04-12 22:22:13 +0800

    refactor: update build config and docs
    - build.sh: add RELEASE flag for production builds
    - build_bash_prompt_mingw64.sh: switch to zig cc compiler
    - cmd.c3: remove test code from production build
    - win.c3: fix debug symbol conditional compilation
    - README.md: update agent description and add mini-pi reference

--- build_bash_prompt_mingw64.sh
+++ build_bash_prompt_mingw64.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-OUTPUT=bash_prompt
+OUTPUT=bash_prompt.exe
 
 c3c compile-only bash_prompt.c3 cmd.c3 --target mingw-x64 --single-module=yes -O2
-gcc -o $OUTPUT ./obj/mingw-x64/bash_prompt.obj -ldbghelp -lshlwapi
-strip -s ${OUTPUT}.exe
+zig cc -o $OUTPUT ./obj/mingw-x64/bash_prompt.obj -ldbghelp -lshlwapi
+strip -s ${OUTPUT}
