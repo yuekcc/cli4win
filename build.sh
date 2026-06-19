568f937675fad2149caefe6f35efae185af9472d
yuekcc <yuekcc@qq.com>
2026-06-19 23:17:24 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -2,15 +2,11 @@
 
 set -ex
 
-sources=(
-    "open.c3"
-    "launch.c3"
-    "bash_prompt.c3"
-)
+echo Build bash_prompt
+c3c compile -D RELEASE -O2 -g0 -o dist/bash_prompt bash_prompt.c3 cmd.c3
 
-for src_file in ${sources[@]};
-do
-    tool_name="${src_file%.*}"
-    echo "build tool: ${tool_name}"
-    c3c compile -D RELEASE -Os -o dist/$tool_name $src_file win.c3 cmd.c3
-done
+echo Build open
+c3c compile -D RELEASE -O2 -g0 -o dist/open open.c3 win.c3
+
+echo Build launch
+c3c compile -D RELEASE -O2 -g0 -o dist/launch launch.c3 win.c3
